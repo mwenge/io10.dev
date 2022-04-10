3e11203b83f5068fa07805e4f14a2a0d9ac57b1b
Robert Hogan <robhogan@gmail.com>
2022-04-10 11:34:56 +0100

    Add a lua example

--- app/example.js
+++ app/example.js
@@ -31,8 +31,10 @@ export const examplePipelines = [
 #       +-----------+-------+
 #  
 #   io10.dev supports Python, SQL, Javascript, R, and Lua.
-#   Everything is run and stored locally in your browser. No data leaves your
-#   computer.
+#
+#   Everything is run and stored locally in your browser.
+#
+#   No data leaves your computer.
 #
 #   Let's start by running the Python program below to generate its output. 
 #    - Press Ctrl-Enter to run the program. 
@@ -225,137 +227,25 @@ print(data)
     `,
     input: ``,
     output: '',
-    lang: "*.py",
+    lang: "*.r",
     files: ['file.csv'],
   },
   {
     key: "Introduction Part Two-1",
-    program: `--  Now we're at the second step of our pipeline:
--- 
---   Notice that the input pane in the top right contains the output of our previous step.
---   Here we'll learn how to run a SQL query on this input.
---  
---                              +--------------------------------------+  
---      +-------+     +-------+ |  +-------+    +-------+    +-------+ |
---      |       |     |       | |  |       |    |       |    |       | |
---      | Input |-----| *.py  |----| Output|----- *.sql |----|Output | |
---      |       |     |       | |  |       |    |       |    |       | |
---      +-------+     +-------+ |  +-------+    +-------+    +-------+ |
---                              +--------------------------------------+
--- 
---  The table containing the input in the top-right pane is always available as a table called "input.txt",
---  so our query is as follows. Press Ctrl-Enter to run the query, then navigate to the
---  the next step by pressing Alt-Right.
---
-SELECT colid, count(*) colsum
-FROM "input.txt" A 
-GROUP BY 1
+    program: `--  Now we're at the second step of our pipeline, this time we use Lua:
+-- Input is available as 'input' local variable.
+local output = {}
+local colors = { "red", "green", "blue" }
+
+for k, v in pairs(colors) do
+  table.insert(output, input[k] .. "\\t" .. k .. ":" .. v .. "\\n")
+end
+table.sort(output)
+return table.concat(output, '')
 `,
     input: ``,
     output: '',
-    lang: "*.sql",
-  },
-  {
-    key: "Introduction Part Two-2",
-    program: `# Welcome to the third step in our pipeline.
-# 
-# This time we're going to use a file as an extra input to the step:
-#
-#
-#                               +--------------------------------------+  
-#       +-------+     +-------+ |  +-------+    +-------+    +-------+ |
-#       |       |     |       | |  |       |    |       |    |       | |
-#       | Input |-----| *.sql |----| Output|----- *.py  |----|Output | |
-#       |       |     |       | |  |       |    |       |    |       | |
-#       +-------+     +-------+ |  +-------+    +-------+    +-------+ |
-#                               |                   |                  |
-#                               |               +-------+              |
-#                               |               |       |              |
-#                               |               | File  |              |
-#                               |               |       |              |
-#                               |               +-------+              |
-#                               +--------------------------------------+
-#
-# The file, 'file.txt', has already been loaded for this example. Loading
-# a file of your own is simple: press Ctrl-O and select the file. It will
-# then be available with it's original name to your Python script.
-#
-import sys
-l = open('file.txt', 'r')
-print("colid" + '\t' + "colval")
-print(l.readline())
-    `,
-    input: ``,
-    output: '',
-    files: ['file.txt'],
-    lang: "*.py",
-  },
-  {
-    key: "Introduction Part Two-3",
-    program: `-- Welcome to the fourth step in our pipeline.
---  
---  We're going to use an extra file again in this step, but this time as 
---  a table in a SQL query.
--- 
---                              +--------------------------------------+  
---      +-------+     +-------+ |  +-------+    +-------+    +-------+ |
---      |       |     |       | |  |       |    |       |    |       | |
---      | Input |-----| *.py  |----| Output|----- *.sql |----|Output | |
---      |       |     |       | |  |       |    |       |    |       | |
---      +-------+     +-------+ |  +-------+    +-------+    +-------+ |
---                              |                   |                  |
---                              |               +-------+              |
---                              |               |       |              |
---                              |               | File  |              |
---                              |               |       |              |
---                              |               +-------+              |
---                              +--------------------------------------+
--- 
---  As in the previous step, the file, 'table.csv', has already been loaded for this example.
--- 
--- Now that you've seen the basics, you can start creating pipelines of your own.
---   - Press Alt-Up to create a new pipeline.
---   - Click on the pipeline name in the bottom left to give it a meaningful name of your own.
---   - Use Alt-Up and Alt-Down to navigate between your pipelines.
---   - Use Alt-A to add a new step to a pipeline.
---   - Use Alt-D to delete the current step from a pipeline.
---   - Use Alt-Left and Alt-Right to navigate between the steps on the current pipeline.
---
-SELECT *
-FROM "table.csv" A 
-`,
-    input: ``,
-    output: '',
-    files: ['table.csv'],
-    lang: "*.sql",
-  },
-  {
-    key: "Introduction Part Two-4",
-    program: `// Welcome to the fifth step in our pipeline.
-//  
-//  This time we're using Javascript.
-// 
-// 
-//                              +--------------------------------------+  
-//      +-------+     +-------+ |  +-------+    +-------+    +-------+ |
-//      |       |     |       | |  |       |    |       |    |       | |
-//      | Input |-----| *.sql |----| Output|----| *.js  |----|Output | |
-//      |       |     |       | |  |       |    |       |    |       | |
-//      +-------+     +-------+ |  +-------+    +-------+    +-------+ |
-//                              +--------------------------------------+
-// 
-// The output from the previous step is stored in an array of strings called 'input'.
-// In this Javascript snippet we filter for entries where 'dept' contains 'IT'.
-// We use the 'return' statement to return the script's output
-// 
-var filtered = input.filter(x => x.split('\t')[4] == 'IT');
-var output =  input[0]+'\n' + filtered.join('\n');
-return output;
-`,
-    input: ``,
-    output: '',
-    files: [],
-    lang: "*.js",
+    lang: "*.lua",
   },
   ]
   },
--- app/lua.js
+++ app/lua.js
@@ -1,9 +1,15 @@
 export async function asyncRunLua(input, code) {
   // Create a standalone lua environment from the factory
   let output = '';
+  let error = '';
+  let input_array = 'local input = {"' + input.split('\n').join('","') + '"}\n';
+  code = input_array + code;
   // Run a lua string
-  output = await fengari.load(code)();
-  console.log("lua output", output)
-  return { output: output, results: '', error: ''};
+  try {
+    output = await fengari.load(code)();
+  } catch(e) {
+    error = e;
+  }
+  return { output: output, results: '', error: error};
 };
 
