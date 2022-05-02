21d8c8825b29a481af8b9027ee86fc1f3b404e0d
Robert Hogan <robhogan@gmail.com>
2022-05-02 10:22:47 +0100

    Experiment with google api

--- main.js
+++ main.js
@@ -34,6 +34,7 @@ editor.setOption("extraKeys", {
       "Ctrl-O": ps.openFile,
       "Ctrl-D": interruptExecution,
       "Ctrl-S": download,
+      "Ctrl-G": uploadToGoogle,
       "Shift-Tab": false,
       "Ctrl-Space": "autocomplete",
     });
@@ -540,3 +541,27 @@ window.onresize = function(e) {
   }
 };
 
+function uploadToGoogle() {
+	var fileContent = 'sample text'; // As a sample, upload a text file.
+	var file = new Blob([fileContent], {type: 'text/plain'});
+	var metadata = {
+			'name': 'sampleName', // Filename at Google Drive
+			'mimeType': 'text/plain', // mimeType at Google Drive
+			'parents': ['### folder ID ###'], // Folder ID at Google Drive
+	};
+
+	var accessToken = gapi.auth.getToken().access_token; // Here gapi is used for retrieving the access token.
+	var form = new FormData();
+	form.append('metadata', new Blob([JSON.stringify(metadata)], { type: 'application/json' }));
+	form.append('file', file);
+
+	fetch('https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id', {
+			method: 'POST',
+			headers: new Headers({ 'Authorization': 'Bearer ' + accessToken }),
+			body: form,
+	}).then((res) => {
+			return res.json();
+	}).then(function(val) {
+			console.log(val);
+	});
+}
