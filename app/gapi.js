9ce08c3b78616f58bca9bf74019aa1956a30e04d
Robert Hogan <robhogan@gmail.com>
2022-05-05 15:34:35 +0100

    Split out google drive logic

--- app/gapi.js
+++ app/gapi.js
@@ -1,6 +1,6 @@
 // Enter an API key from the Google API Console:
 //   https://console.developers.google.com/apis/credentials
-var apiKey = 'AIzaSyCmrP2EaZ1947hTLfVdNL2tS9-6R1hHL9o';
+var apiKey = 'AIzaSyDAJMKenlcYQjhQnt6o8yhaWWiIH2De_Yo';
 
 // Enter the API Discovery Docs that describes the APIs you want to
 // access. In this example, we are accessing the People API, so we load
@@ -11,26 +11,16 @@ var discoveryDocs = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest
 //   https://console.developers.google.com/apis/credentials?project=_
 // In your API Console project, add a JavaScript origin that corresponds
 //   to the domain where you will be running the script.
-var clientId = '814095371108-5t63a26bqn3q0l1cd8e3pkctjaacn8pm.apps.googleusercontent.com';
+var clientId = '1026743788615-mic0188b618am7ln8q76blu9pilt1ic9.apps.googleusercontent.com';
 
 // Enter one or more authorization scopes. Refer to the documentation for
 // the API or https://developers.google.com/people/v1/how-tos/authorizing
 // for details.
-var scopes = 'https://www.googleapis.com/auth/drive.file ';
+var scopes = 'https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.appdata';
 
 function handleClientLoad() {
   // Load the API client and auth2 library
   gapi.load('client:auth2', initClient);
-}
-
-let tokenClient;
-function gisLoaded() {
-	tokenClient = google.accounts.oauth2.initTokenClient({
-		client_id: clientId,
-		scope: scopes,
-		callback: '', // defined later
-	});
-	gisInited = true;
 }
 
 function initClient() {
@@ -41,3 +31,4 @@ function initClient() {
     scope: scopes
   });
 }
+
--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import {setUpOutput} from "./app/output.js";
 import * as ps from "./app/pipelines.js";
 import { asyncRunJS } from "./app/js.js";
 import { asyncRunLua } from "./app/lua.js";
+import { savePipelineToGoogleDrive } from "./app/gdrive.js";
 
 const cmLangs = {
   "*.py" : { lang: "*.py",  syntax: "text/x-python", run: evaluatePython, interrupt: interruptPythonExecution},
@@ -552,62 +553,7 @@ async function uploadToGoogle() {
   running.style.display = "block";
   running.textContent = "Creating Zip File";
   let zipFile = await createZipFile();
-  let zipFileName = ps.pipelinePrettyName() + ".zip";
+  let zipFileName = "io10.dev - " + ps.pipelinePrettyName() + ".zip";
 
-  running.textContent = "Signing into Google";
-  if (!gapi.auth2.getAuthInstance().isSignedIn.get()) {
-    gapi.auth2.getAuthInstance().signIn();
-  }
-
-	// Get the folder ID for 'io10' and store the zip in that.
-	let folderName = "io10";
-	let result = await gapi.client.drive.files.list({
-			q: "mimeType='application/vnd.google-apps.folder' and trashed=false",
-			fields: 'nextPageToken, files(id, name)',
-			spaces: 'drive',
-	});
-	let folder = result.result.files.filter(x => x.name === folderName);
-	var folderId = folder.length?folder[0].id:0;
-	// 'io10' folder doesn't exist: create it.
-	if (!folderId) {
-		var fileMetadata = {
-			'name': 'io10',
-			'mimeType': 'application/vnd.google-apps.folder'
-		};
-		let createResult = await gapi.client.drive.files.create({
-			resource: fileMetadata,
-			fields: 'id'
-		});
-		console.log('Folder Id: ', createResult);
-		folderId = createResult.id;
-	}
-
-	// Store the zip file.
-	var file = new Blob([zipFile], {type: 'application/zip'});
-	var metadata = {
-			'name': zipFileName, // Filename at Google Drive
-			'mimeType': 'application/zip', // mimeType at Google Drive
-			'parents': [folderId], // Folder ID at Google Drive
-	};
-
-  running.textContent = "Uploading to Google";
-	var accessToken = gapi.auth.getToken().access_token; // Here gapi is used for retrieving the access token.
-	var form = new FormData();
-	form.append('metadata', new Blob([JSON.stringify(metadata)], { type: 'application/json' }));
-	form.append('file', file);
-
-	fetch('https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id', {
-			method: 'POST',
-			headers: new Headers({ 'Authorization': 'Bearer ' + accessToken }),
-			body: form,
-	}).then((response) => {
-      if (response.status >= 200 && response.status <= 299) {
-        running.textContent = `Saved '${zipFileName}' to Google Drive`;
-        return response.json();
-      } else {
-        throw Error(response.statusText);
-      }
-  }).catch((error) => {
-      running.textContent = `Error Saving '${zipFileName}' to Google Drive: ${error}`;
-	});
+  savePipelineToGoogleDrive(zipFile, zipFileName, running);
 }
