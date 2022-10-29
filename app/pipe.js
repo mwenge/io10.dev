ca65825a688e38e49e52c7c18d2cca0d5b6e5329
Robert Hogan <robhogan@gmail.com>
2022-10-29 14:17:03 +0100

    Use random tips

--- app/pipe.js
+++ app/pipe.js
@@ -1,71 +1,16 @@
 // A pipe is a program/script with its input and output. A sequence of pipes
 // make up a pipeline.
 import * as storage from "./storage.js";
+import * as tips from "./tips.js";
 const defaultData = 
   {
-    program: `# Keyboard Commands.
-#  Current Step
-#    - Ctrl-Enter: Run the current step.
-#    - Ctrl-D:     Interrupt execution (Python, R, and SQL only)
-#
-#  Current Pipeline
-#    - Alt-R:     Run the entire pipeline from the start.
-#    - Alt-Left:  Navigate to the previous step in the pipeline.
-#    - Alt-Right: Navigate to the next step in the pipeline.
-#    - Alt-A:     Add a new step after the current one.
-#    - Alt-B:     Add a new step before the current one.
-#    - Alt-C:     Delete the current step.
-#    - Ctrl-O:    Add a file to the current step.
-#
-#  Saving and Loading Pipelines
-#    - Ctrl-S:    Download and save the current pipeline as a zip file.
-#    - Ctrl-O:    Select and load a previously saved zip file.
-#
-#  Synchronizing with Google Drive
-#    - Alt-G:     Save/Update the current pipeline on Google Drive.
-#    - Alt-L:     Load all saved pipelines from Google Drive. This won't
-#                 overwrite any open pipelines with the same name.
-#    NOTE: The first attempt to connect to Google Drive may fail. If that
-#    happens just try again, it usually works on the second attempt.
-#
-#  When Cursor in Input/Output panes
-#    - PageUp/
-#      PageDown:  Page up and down through input/output.
-#    - Ctrl-Home: Move back to start of input/output
-#
-#  Other Pipelines
-#    - Alt-Up:   Create a new pipeline or navigate to the next pipeline.
-#    - Alt-Down: Navigate to the previous pipeline.
-#    - Click on the pipeline name in the bottom left to give it a
-#      meaningful name of your own.
-#
-#  Language Detection
-#  io10.dev will automatically detect the language you're using, most of the time.
-#  For short one-liners, you can force language detection by using a 'shebang' on the
-#  first line of the script, e.g.:
-#
-#    #!/bin/python
-#    x = 9
-#    print(x)
-#
-#  The available shebangs are:
-#
-#    #!/bin/python
-#    #!/bin/py
-#    #!/bin/javascript
-#    #!/bin/js
-#    #!/bin/sql
-#    #!/bin/r
-#    #!/bin/lua
-#    #!/bin/awk
-
-
+    program: `
 import sys
 w = sys.stdin.readline();
 print("colid" + '\t' + "colval")
 for i in range(0,20):
   print(str(1) + '\t' + w.strip() +str(i))
-    `,
+`,
     input: ``,
     output: '',
     lang: "*.py",
@@ -75,7 +20,8 @@ async function getPipe(prevID, id) {
   let data = await storage.getData(id);
   if (!data) {
     data = {};
-    data.program = defaultData.program;
+    let randomTip = Math.floor(Math.random() * tips.tips.length);
+    data.program = tips.tips[randomTip] + defaultData.program;
     data.input = defaultData.input;
     data.output = defaultData.output;
     data.lang = defaultData.lang;
---
+++ app/tips.js
@@ -0,0 +1,78 @@
+ export const tips = [
+`# Here's a tip:
+#  Current Step
+#    - Ctrl-Enter: Run the current step.
+#    - Ctrl-D:     Interrupt execution (Python, R, and SQL only)
+`,
+`# Here's a tip:
+#
+#  Current Pipeline
+#    - Alt-R:     Run the entire pipeline from the start.
+`,
+`# Here's a tip:
+#    - Alt-Left:  Navigate to the previous step in the pipeline.
+#    - Alt-Right: Navigate to the next step in the pipeline.
+`,
+`# Here's a tip:
+#    - Alt-A:     Add a new step after the current one.
+#    - Alt-B:     Add a new step before the current one.
+`,
+`# Here's a tip:
+#    - Alt-C:     Delete the current step.
+`,
+`# Here's a tip:
+#    - Ctrl-O:    Add a file to the current step.
+#
+`,
+`# Here's a tip:
+#    - Ctrl-S:    Download and save the current pipeline as a zip file.
+`,
+`# Here's a tip:
+#    - Ctrl-O:    Select and load a previously saved zip file.
+#
+`,
+`# Here's a tip:
+#  Synchronizing with Google Drive
+#    - Alt-G:     Save/Update the current pipeline on Google Drive.
+#    - Alt-L:     Load all saved pipelines from Google Drive. This won't
+#                 overwrite any open pipelines with the same name.
+#    NOTE: The first attempt to connect to Google Drive may fail. If that
+#    happens just try again, it usually works on the second attempt.
+#
+`,
+`# Here's a tip:
+#  When Cursor in Input/Output panes
+#    - PageUp/
+#      PageDown:  Page up and down through input/output.
+#    - Ctrl-Home: Move back to start of input/output
+#
+`,
+`# Here's a tip:
+#  Other Pipelines
+#    - Alt-Up:   Create a new pipeline or navigate to the next pipeline.
+#    - Alt-Down: Navigate to the previous pipeline.
+`,
+`# Here's a tip:
+#    - Click on the pipeline name in the bottom left to give it a
+#      meaningful name of your own.
+`,
+`# Here's a tip:
+#  io10.dev will automatically detect the language you're using, most of the time.
+#  For short one-liners, you can force language detection by using a 'shebang' on the
+#  first line of the script, e.g.:
+#
+#    #!/bin/python
+#    x = 9
+#    print(x)
+#
+#  The available shebangs are:
+#
+#    #!/bin/python
+#    #!/bin/py
+#    #!/bin/js
+#    #!/bin/sql
+#    #!/bin/r
+#    #!/bin/lua
+#    #!/bin/awk
+`,
+ ];
