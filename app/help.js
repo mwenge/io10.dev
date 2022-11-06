5e990802dacaf9019b13dd2a3c72ce6a8714c224
Robert Hogan <robhogan@gmail.com>
2022-11-06 10:28:20 +0000

    Make position of tips consistent

--- app/help.js
+++ app/help.js
@@ -4,7 +4,7 @@ const tips = [
 "<div class=\"lozenge lozenge-alt\">Tip: <kbd class=\"alt\">Alt</kbd>+<kbd>A/B</kbd> Insert Step After/Before</div>",
 "<div class=\"lozenge lozenge-alt\">Tip: <kbd class=\"ctrl\">Ctrl</kbd>+<kbd>D</kbd> Interrupt Execution</div>",
 "<div class=\"lozenge lozenge-alt\">Tip: <kbd class=\"alt\">Alt</kbd>+<kbd>R</kbd> Run Pipeline</div>",
-"<div class=\"lozenge lozenge-alt\">Tip: <kbd class=\"ctrl\">Ctrl</kbd>+<kbd>O</kbd> Add File or Load Saved Pipeline</div>",
+"<div class=\"lozenge lozenge-alt\">Tip: <kbd class=\"ctrl\">Ctrl</kbd>+<kbd>O</kbd> Load File/Saved Pipeline</div>",
 "<div class=\"lozenge lozenge-alt\">Tip: <kbd class=\"ctrl\">Ctrl</kbd>+<kbd>S</kbd> Save Pipeline as Zip File</div>",
 "<div class=\"lozenge lozenge-alt\">Tip: <kbd class=\"alt\">Alt</kbd>+<kbd>&#8595;/&#8593;</kbd> Previous/Next Pipeline</div>",
 "<div class=\"lozenge\">Tip: <kbd class=\"alt\">Alt</kbd>+<kbd>C</kbd> Delete Current Step</div>",
