9c4090102dabf8d578dcd6f9a1b5d1976b27d663
Rob Hogan <robhogan@gmail.com>
2023-08-31 16:04:36 +0300

    Twiddle with the colors

--- app/help.js
+++ app/help.js
@@ -14,16 +14,16 @@ const keyboard_shortcuts = [
 
 const tips = [
 "<div class=\"lozenge\">Tip: <kbd class=\"ctrl\">Ctrl</kbd>+<kbd class=\"enter\">Enter</kbd> Run Current Step</div>",
-"<div class=\"lozenge lozenge-alt\">Tip: <kbd class=\"alt\">Alt</kbd>+<kbd>&#8592;/&#8594;</kbd> Previous/Next Step</div>",
-"<div class=\"lozenge lozenge-alt\">Tip: <kbd class=\"alt\">Alt</kbd>+<kbd>R</kbd> Run Pipeline</div>",
-"<div class=\"lozenge lozenge-alt\">Tip: <kbd class=\"alt\">Alt</kbd>+<kbd>A/B</kbd> Insert Step After/Before</div>",
-"<div class=\"lozenge lozenge-alt\">Tip: <kbd class=\"ctrl\">Ctrl</kbd>+<kbd>O</kbd> Load File</div>",
-"<div class=\"lozenge lozenge-alt\">Tip: <kbd class=\"alt\">Alt</kbd>+<kbd>&#8595;/&#8593;</kbd> Previous/Next Pipeline</div>",
+"<div class=\"lozenge \">Tip: <kbd class=\"alt\">Alt</kbd>+<kbd>&#8592;/&#8594;</kbd> Previous/Next Step</div>",
+"<div class=\"lozenge \">Tip: <kbd class=\"alt\">Alt</kbd>+<kbd>R</kbd> Run Pipeline</div>",
+"<div class=\"lozenge \">Tip: <kbd class=\"alt\">Alt</kbd>+<kbd>A/B</kbd> Insert Step After/Before</div>",
+"<div class=\"lozenge \">Tip: <kbd class=\"ctrl\">Ctrl</kbd>+<kbd>O</kbd> Load File</div>",
+"<div class=\"lozenge \">Tip: <kbd class=\"alt\">Alt</kbd>+<kbd>&#8595;/&#8593;</kbd> Previous/Next Pipeline</div>",
 "<div class=\"lozenge\">Tip: <kbd class=\"alt\">Alt</kbd>+<kbd>C</kbd> Delete Current Step</div>",
 "<div class=\"lozenge\">Tip: <kbd class=\"alt\">Alt</kbd>+<kbd>Q</kbd> Delete Pipeline</div>",
-"<div class=\"lozenge lozenge-alt\">Tip: <kbd class=\"ctrl\">Ctrl</kbd>+<kbd>D</kbd> Interrupt Execution</div>",
-"<div class=\"lozenge lozenge-alt\">Tip: <kbd class=\"ctrl\">Ctrl</kbd>+<kbd>O</kbd> Load Pipeline</div>",
-"<div class=\"lozenge lozenge-alt\">Tip: <kbd class=\"ctrl\">Ctrl</kbd>+<kbd>S</kbd> Save Pipeline as Zip File</div>",
+"<div class=\"lozenge \">Tip: <kbd class=\"ctrl\">Ctrl</kbd>+<kbd>D</kbd> Interrupt Execution</div>",
+"<div class=\"lozenge \">Tip: <kbd class=\"ctrl\">Ctrl</kbd>+<kbd>O</kbd> Load Pipeline</div>",
+"<div class=\"lozenge \">Tip: <kbd class=\"ctrl\">Ctrl</kbd>+<kbd>S</kbd> Save Pipeline as Zip File</div>",
 "<div class=\"lozenge\">Tip: Click on pipeline name to change it.</div>",
 ];
 
